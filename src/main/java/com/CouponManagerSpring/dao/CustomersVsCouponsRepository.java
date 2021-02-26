38e6468f35d55ba5947f3170f2ede359806ce47e
user <user@DESKTOP-UVKKN7B>
2021-02-27 00:06:18 +0200

    שדג/'ג/'ג/'ג/'ג

--- src/main/java/com/CouponManagerSpring/dao/CustomersVsCouponsRepository.java
+++ src/main/java/com/CouponManagerSpring/dao/CustomersVsCouponsRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomersVsCouponsRepository extends JpaRepository<CustomersVsCoupons,CustomerVsCouponMultipleId > {
 	
-	
-	
 	public List<CustomersVsCoupons> findByCouponId(int couponID);
 	public List<CustomersVsCoupons> findByCustomerId(int customerID);
 	public void deleteByCouponId(int couponID);
